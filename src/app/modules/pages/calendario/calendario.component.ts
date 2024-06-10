c97344015dcb47957c0596d4c02b1dce0e3a116b
Alisson <alissonpokrysilva@gmail.com>
2024-06-09 22:42:17 -0300

    Calendar card done

--- src/app/modules/pages/calendario/calendario.component.ts
+++ src/app/modules/pages/calendario/calendario.component.ts
@@ -6,6 +6,7 @@ import { CalendarModule } from 'primeng/calendar';
 import { AulaInterface } from '../../interfaces/aula.interface';
 import { AulasService } from '../../services/aulas.service';
 
+
 @Component({
     selector: 'app-calendario',
     standalone: true,
@@ -19,6 +20,19 @@ export class CalendarioComponent {
     diaNome: string | undefined;
     diaNumero: string | undefined;
     isDateSelected = false;
+    isClassDay = false
+
+    aulas: AulaInterface[] = [];
+    selectedAula: AulaInterface | undefined;
+    
+    diaAula: string | undefined;
+    mesAula: string | undefined;
+    anoAula: string | undefined;
+
+    salaAula: number | undefined
+    materiaAula: string | undefined
+    blocoAula: string | undefined
+    professorAula: string | undefined
     
     selectDate() {
         let dateString = this.date?.toString();
@@ -28,19 +42,39 @@ export class CalendarioComponent {
             this.diaNome = splittedDate[0];
             this.diaNumero = splittedDate[2];
             this.isDateSelected = true;
+
+            this.selectedAula = this.aulas.find((aula) => {
+                let splittedAulaDate = aula.data.split("-")
+
+                this.diaAula = splittedAulaDate[2].split("T")[0]
+                this.mesAula = splittedAulaDate[1]
+                this.anoAula = splittedAulaDate[0]
+
+                if (this.diaNumero == this.diaAula) {
+                    this.materiaAula = aula.materia.name
+                    this.blocoAula = aula.sala.bloco.letraBloco
+                    this.salaAula = aula.sala.number
+                    this.professorAula = aula.materia.professor.name
+                    this.isClassDay = true
+                } else {
+                    this.isClassDay = false
+                }
+              });
         }
     }
 
     //Exibir no card informações sobre a aula daquele dia
-    aulas: AulaInterface[] = [];
-    selectedAula: AulaInterface | undefined;
-
     constructor(private service: AulasService) { }
-    
+
     ngOnInit(): void {
         this.service.getAulasByStudentId(1).subscribe((data) => {
           console.log(data);
           this.aulas = data;
         });
+      }
+
+      showClassInfo(index: number): void {
+        console.log(index);
+        this.selectedAula = this.aulas[index];
       }
 }
