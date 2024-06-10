0b29815b02cbcdce3526341f86db5c4e7f07c5bc
Artur Costa <souza.artur@edu.univali.br>
2024-06-10 13:07:20 -0300

    Add login validation

--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -4,16 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import jwt_decode, { jwtDecode } from 'jwt-decode';
+import { Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MessagesModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  messages: Message[] | undefined;
   codigoPessoa: string = '';
   password: string = '';
 
@@ -21,19 +23,30 @@ export class LoginComponent {
 
   goToItems() {
     console.log(this.codigoPessoa);
-    this.service.login(this.codigoPessoa, this.password).subscribe((userInfo) => {
-      if(userInfo) {
-        const decodedToken : any = jwtDecode(userInfo.token);
-        if(decodedToken.role === "Student") {
-          this.router.navigate(['/calendar-page']);
+    if(this.codigoPessoa && this.password) {
+      this.service.login(this.codigoPessoa, this.password).subscribe((userInfo) => {
+        if(userInfo) {
+          const decodedToken : any = jwtDecode(userInfo.token);
+          if(decodedToken.role === "Student") {
+            this.router.navigate(['/calendar-page']);
+          }
+          else if (decodedToken.role === "Professor") {
+            this.router.navigate(['/disciplines-page']);
+          } else {
+            this.router.navigate(['/aluno-page']);
+          }
         }
-        else if (decodedToken.role === "Professor") {
-          this.router.navigate(['/disciplines-page']);
-        } else {
-          this.router.navigate(['/aluno-page']);
-        }
-      }
-    });
+      });
+    } else if(this.codigoPessoa !== '' && this.codigoPessoa != "11111111" && this.codigoPessoa != "22222222" && this.codigoPessoa != "33333333") {
+
+      this.messages = [{severity: 'error', summary: '', detail: 'Informe um código de pessoa válido'}];
+    }
+    else if (this.codigoPessoa != '' && this.password != "senha123") {
+      this.messages = [{severity: 'error', summary: '', detail: 'Informe a senha correta'}];
+    } else {
+      this.messages = [{severity: 'error', summary: '', detail: 'Informe login e senha'}];
+    }
+
   }
 
   visitor() {
