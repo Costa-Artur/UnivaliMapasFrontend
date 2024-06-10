b5dd055ae97a47b4378927b0abb9e81e613a0f24
Alisson <alissonpokrysilva@gmail.com>
2024-06-10 00:45:39 -0300

    Map dropdowns changed

--- src/app/modules/components/block-dropdown/block-dropdown.component.ts
+++ src/app/modules/components/block-dropdown/block-dropdown.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
 interface Block {
   id: number;
@@ -9,45 +11,26 @@ interface Block {
 @Component({
   selector: 'app-block-dropdown',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, DropdownModule],
   templateUrl: './block-dropdown.component.html',
   styleUrl: './block-dropdown.component.scss'
 })
 export class BlockDropdownComponent {
-  blocks: Block[] = [
-    { id: 1, name: 'B1' },
-    { id: 2, name: 'B2' },
-    { id: 3, name: 'B3' },
-    { id: 4, name: 'B4' },
-    { id: 5, name: 'B5' },
-    { id: 6, name: 'B6' },
-    { id: 7, name: 'B7' },
-    { id: 8, name: 'C1' },
-    { id: 9, name: 'C2' },
-    { id: 10, name: 'C3' },
-    { id: 11, name: 'C4' },
-    { id: 12, name: 'C5' },
-    { id: 13, name: 'D1' },
-    { id: 14, name: 'D2' },
-    { id: 15, name: 'D3' },
-    { id: 16, name: 'D4' },
-    { id: 17, name: 'D5' },
-    { id: 18, name: 'D6' },
-    { id: 19, name: 'D7' },
-    { id: 20, name: 'D8' },
-    { id: 21, name: 'D9' },
-    { id: 22, name: 'D10' },
-    { id: 23, name: 'E1' },
-    { id: 24, name: 'E2' },
-    { id: 25, name: 'E3' },
-    { id: 26, name: 'F1' }, 
-    { id: 27, name: 'F2' }, 
-    { id: 28, name: 'F3' }, 
-    { id: 29, name: 'F4' }, 
-    { id: 30, name: 'F5' }, 
-    { id: 31, name: 'F6' }, 
-    { id: 32, name: 'F7' }
-  ];
+  blocks: Block[] | undefined
+  selectedBlock: Block | undefined
 
-  selectedBlock: Block | undefined;
+  ngOnInit() {
+    this.blocks = [
+    { id: 1, name: 'B1' },
+    { id: 4, name: 'B4' },
+    { id: 6, name: 'B6' },
+    { id: 10, name: 'C3' },
+    { id: 12, name: 'C5' },
+    { id: 16, name: 'D4' },
+    ]
+  }
+
+  onSelectedBlock() {
+    console.log(this.selectedBlock?.name);
+  }
 }
--- src/app/modules/components/class-dropdown/class-dropdown.component.ts
+++ src/app/modules/components/class-dropdown/class-dropdown.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
 interface Class {
   id: number;
@@ -9,33 +11,40 @@ interface Class {
 @Component({
   selector: 'app-class-dropdown',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, DropdownModule],
   templateUrl: './class-dropdown.component.html',
   styleUrl: './class-dropdown.component.scss'
 })
 export class ClassDropdownComponent {
-  classes: Class[] = [
-    { id: 1, number: 101 },
-    { id: 2, number: 102 },
-    { id: 3, number: 103 },
-    { id: 4, number: 104 },
-    { id: 5, number: 105 },
-    { id: 6, number: 106 },
-    { id: 7, number: 107 },
-    { id: 8, number: 108 },
-    { id: 9, number: 109 },
-    { id: 10, number: 110 },
-    { id: 11, number: 201 },
-    { id: 12, number: 202 },
-    { id: 13, number: 203 },
-    { id: 14, number: 204 },
-    { id: 15, number: 205 },
-    { id: 16, number: 206 },
-    { id: 17, number: 207 },
-    { id: 18, number: 208 },
-    { id: 19, number: 209 },
-    { id: 20, number: 210 },
-  ];
+  classes: Class[] | undefined
+  selectedClass: Class | undefined
 
-  selectedClass: Class | undefined;
+  ngOnInit() {
+    this.classes = [
+      { id: 1, number: 101 },
+      { id: 2, number: 102 },
+      { id: 3, number: 103 },
+      { id: 4, number: 104 },
+      { id: 5, number: 105 },
+      { id: 6, number: 106 },
+      { id: 7, number: 107 },
+      { id: 8, number: 108 },
+      { id: 9, number: 109 },
+      { id: 10, number: 110 },
+      { id: 11, number: 201 },
+      { id: 12, number: 202 },
+      { id: 13, number: 203 },
+      { id: 14, number: 204 },
+      { id: 15, number: 205 },
+      { id: 16, number: 206 },
+      { id: 17, number: 207 },
+      { id: 18, number: 208 },
+      { id: 19, number: 209 },
+      { id: 20, number: 210 },
+    ]
+  }
+
+  onSelectedClass() {
+    console.log(this.selectedClass?.number);
+  }
 }
